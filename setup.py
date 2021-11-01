28d9bff25e5eaeacb7215f7cbb1f3e941bd3adc3
TaceyWong <xinyong.w@gmail>
2021-11-02 02:40:36 +0800

    add deps

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', ]
+requirements = ['Click>=7.0', 'html2text>=2020.1.16',
+'Jinja2>=3.0.2']
 
 test_requirements = ['pytest>=3', ]
 
